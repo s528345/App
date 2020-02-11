399f410f90850fd4801ebd018470408c66e6058d
s528345 <42850453+s528345@users.noreply.github.com>
2020-02-11 13:36:25 -0600

    Scripting

---
+++ scripts.js
@@ -0,0 +1,23 @@
+console.log('INFO: Starting to read scripts.js');
+
+console.log('INFO: Getting access to elements');
+
+function calculate() {
+
+    const total = parseFloat(document.getElementById("total").value);
+    const tip = parseFloat(document.getElementById("tip").value);
+    const result = (total*0.055) + tip;
+    const result2 = parseFloat(result.toFixed(2)); 
+    if(isNaN(result2)){
+        document.getElementById("answer").style.color = "red";
+        answer.innerHTML = "Enter some valid input";
+    }
+    else{
+        document.getElementById("answer").style.color = "blue";
+        answer.innerHTML = `Your total is: $${result2}`;
+    }
+   
+
+
+}
+
