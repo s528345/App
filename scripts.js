f95dd3d43e971acc378dd0c90aef20ef2b731bc0
s528345 <42850453+s528345@users.noreply.github.com>
2020-02-17 13:37:09 -0600

    More Finishing Touches

--- scripts.js
+++ scripts.js
@@ -7,7 +7,10 @@ function calculate() {
     const total = parseFloat(document.getElementById("total").value);
     const tip = parseFloat(document.getElementById("tip").value);
     const result = (total + (total*0.055) + tip) + 0.00;
-    const result2 = parseFloat(result.toFixed(2)); 
+    const result2 = parseFloat(result.toFixed(2));
+    total1.innerHTML = `Total: $${total}`;
+    tip1.innerHTML = `Tip: $${tip}`;
+    GT.innerHTML = `Grand Total: $${result2}`;
     if(isNaN(result2)){
         document.getElementById("answer").style.color = "red";
         answer.innerHTML = "Enter some valid input";
